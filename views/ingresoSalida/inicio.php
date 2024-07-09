01ff1a5ef7287cb1f70e0f74f71a6437ae6df31a
DonIsaac117 <143756049+DonIsaac117@users.noreply.github.com>
2024-07-09 17:38:57 -0500

    Update inicio.php

--- views/ingresoSalida/inicio.php
+++ views/ingresoSalida/inicio.php
@@ -57,7 +57,7 @@ $listadoIngresoSalida = $ingresoSalidaController->listar();
         <label for="estado">estado</label>
         <input type="text" name="estado" id="estado" required>
         
-        <button type="submit">Registrar</button>
+        <button type="submit">Actualizar</button>
     </form>
 
 <h3>Listado de Tabla Ingreso/Salida</h3>
