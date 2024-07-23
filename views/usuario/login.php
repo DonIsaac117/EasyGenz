cd9a360ca13854fafb3470a48862a15e4c53e453
DonIsaac117 <143756049+DonIsaac117@users.noreply.github.com>
2024-07-23 13:39:11 -0500

    Add files via upload

--- views/usuario/login.php
+++ views/usuario/login.php
@@ -1,4 +1,3 @@
-<!-- views/usuario/login.php -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,12 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="./css/login.css">
-    </script>
+    <link rel="stylesheet" href="./css/Login.css">
 </head>
 <body>
     <div>
-        <form action="" method="post">
+        <form action="index.php?vista=usuario/login" method="post">
             <h1>Login</h1>
             <div class="formulario">
                 <h2>Documento</h2>
@@ -21,7 +19,7 @@
                 <div class="olvide">
                     <a href="index.php?vista=usuario/recuperar"><h4>Olvide mi contraseña</h4></a>
                 </div>
-                <input type="submit" value="Ingresar">
+                <input type="submit" name="login_submit" value="Ingresar">
         </form>
         
         <div class="registro"> 
@@ -31,6 +29,47 @@
 
         </div>
     </div>
+
+    <div id="myModal" class="modal">
+        <div class="modal-content">
+            <?php
+            if (isset($_GET['error'])) {
+                $error = $_GET['error'];
+                if ($error == 'credenciales_incorrectas') {
+                    echo 'Credenciales incorrectas';
+                } elseif ($error == 'usuario_no_existe') {
+                    echo 'El usuario no existe en la base de datos';
+                } else {
+                    echo 'Error desconocido';
+                }
+            }
+            ?>
+            <span class="close">&times;</span>
+        </div>
+    </div>
+
+    <script>
+       document.addEventListener('DOMContentLoaded', function() {
+           var modal = document.getElementById('myModal');
+
+           <?php
+           if (isset($_GET['error'])) {
+               echo "modal.style.display = 'block';";
+           }
+           ?>
+
+            var closeBtn = document.querySelector('.close');
+            closeBtn.addEventListener('click', function() {
+                modal.style.display = 'none';
+            });
+
+            window.addEventListener('click', function(event) {
+                if (event.target === modal) {
+                    modal.style.display = 'none';
+                }
+            });
+       });
+   </script>
 
 </body>
 </html>
