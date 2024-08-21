8182330405287378471c1a18d2276d37e4579b0f
JBio2585 <143755922+JBio2585@users.noreply.github.com>
2024-08-21 14:19:30 -0500

    Add files via upload
    ARREGLO DEL BOTON tyc

--- views/usuario/TYC.php
+++ views/usuario/TYC.php
@@ -25,7 +25,7 @@
                 <span>Para consultas relacionadas con el manejo de datos personales, por favor contacte a nuestro delegado de protección de datos en <a href="#">isigenz@gmail.com</a>.</span>
             </ul>
         </p>
-        <a href="index.php?vista=usuario/registrar"><button>Entendidou</button></a>
+        <a class="boton" href="index.php?vista=usuario/registrar">ENTENDIDO</a>
     </div></div>
   
 
