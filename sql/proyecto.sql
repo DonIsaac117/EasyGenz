311dc87df1ade29754543a808f3f63cf1b58e391
JDavid237 <143755841+JDavid237@users.noreply.github.com>
2024-08-27 12:02:44 -0500

    Update proyecto.sql
    cambio al trigger eliminar usuario

--- sql/proyecto.sql
+++ sql/proyecto.sql
@@ -647,14 +647,18 @@ $$
 DELIMITER ;
 DELIMITER $$
 CREATE TRIGGER `eliminar_usuario_total` BEFORE DELETE ON `usuarios` FOR EACH ROW BEGIN
-    -- Eliminar registros relacionados en la tabla 'aprendiz'
+  	
+    -- Eliminar registros relacionados en la tabla 'ingresosalida_ficha'
+    DELETE FROM ingresosalida_ficha WHERE id_usuario = OLD.id;
+
+   -- Eliminar registros relacionados en la tabla 'usuario_perfil'
+    DELETE FROM usuario_perfil WHERE id_usuario = OLD.id;
+
+-- Eliminar registros relacionados en la tabla 'aprendiz'
     DELETE FROM aprendiz WHERE id_usuario = OLD.id;
     
     -- Eliminar registros relacionados en la tabla 'instructor'
     DELETE FROM instructor WHERE id_usuario = OLD.id;
-    
-    -- Eliminar registros relacionados en la tabla 'usuario_perfil'
-    DELETE FROM usuario_perfil WHERE id_usuario = OLD.id;
     
     -- Eliminar registros relacionados en la tabla 'controlfuncionarios'
     DELETE FROM controlfuncionarios WHERE id_usuario = OLD.id;
@@ -664,8 +668,6 @@ CREATE TRIGGER `eliminar_usuario_total` BEFORE DELETE ON `usuarios` FOR EACH ROW
      DELETE FROM funcionario WHERE id_usuario = OLD.id;
     
     
-    -- Eliminar registros relacionados en la tabla 'ingresosalida_ficha'
-    DELETE FROM ingresosalida_ficha WHERE id_usuario = OLD.id;
  END
 $$
 DELIMITER ;
